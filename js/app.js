07a1c04f9ba422f8b0ff2111d7b85975c04f8a1d
Luca Ricci <theskinnyghost@gmail.com>
2017-04-17 18:57:56 +0200

    Add levels and player lives

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/** Set-up game constants */
 var CANVAS_WIDTH = 707,
     CANVAS_HEIGHT = 606,
     COLUMN_WIDTH = 101,
@@ -7,6 +8,110 @@ var CANVAS_WIDTH = 707,
     LIMIT_RIGHT = 606,
     LIMIT_BOTTOM = 404,
     LIMIT_LEFT = 0;
+
+/**
+ * Game class constructor
+ */
+var Game = function(){
+    this.state = true;
+    this.gameOver = false;
+    this.level = 1;
+
+    this.enemies = [];
+    this.setEnemies();
+    this.player = new Player(303, 404);
+
+    this.renderGameInfo();
+};
+
+Game.prototype.setEnemies = function() {
+    for(var i = 1; i < 4; i++){
+        this.enemies.push(new Enemy(0 - (i * COLUMN_WIDTH), i * ROW_HEIGHT));
+    }
+    for(var i = 1; i < 4; i++){
+        this.enemies.push(new Enemy(0 - (i * COLUMN_WIDTH), i * ROW_HEIGHT));
+    }
+    for(var i = 1; i < 4; i++){
+        this.enemies.push(new Enemy(0 - (i * COLUMN_WIDTH), i * ROW_HEIGHT));
+    }
+};
+
+Game.prototype.renderGameInfo = function() {
+    var playerLives = document.getElementById('playerLives'),
+        gameLevel   = document.getElementById('gameLevel');
+
+    playerLives.innerHTML = this.player.lives;
+    gameLevel.innerHTML = this.level;
+};
+
+// Handle key input and move player accordingly
+// check if player has reached one of canvas' side
+// if so, stop moving it
+Game.prototype.handleInput = function(key) {
+    switch(key) {
+        case 'up':
+            if(player.y < ROW_HEIGHT && this.state){
+                player.reset();
+                this.changeLevel();
+            } else if(player.y > LIMIT_TOP && this.state){
+                player.y -= ROW_HEIGHT;
+            }
+            break;
+
+        case 'down':
+            if(player.y < LIMIT_BOTTOM && this.state)
+                player.y += ROW_HEIGHT;
+            break;
+
+        case 'left':
+            if(player.x > LIMIT_LEFT && this.state)
+                player.x -= COLUMN_WIDTH;
+            break;
+
+        case 'right':
+            if(player.x < LIMIT_RIGHT && this.state)
+                player.x += COLUMN_WIDTH;
+            break;
+        case 'pause':
+            if(!this.gameOver)
+                this.toggleState();
+            break;
+    }
+};
+
+Game.prototype.toggleState = function() {
+    this.state = !this.state;
+};
+
+Game.prototype.changeLevel = function(){
+    if(this.level == 4){
+        // Reach end of game
+        this.end();
+    } else {
+        // else level up
+        this.level++;
+        this.renderGameInfo();
+    }
+};
+
+Game.prototype.end = function() {
+    var body = document.getElementsByTagName('body');
+
+    if( (player.lives == 0 && game.level == 4) || player.lives > 1 ) {
+        this.state = false;
+        this.gameOver = true;
+
+        body[0].innerHTML = "<h1>Congratulations, you made it!</h1>" +
+            "<p>You've finished the game, you should be happy. " +
+            "If you want to play again simply refresh the page.</p>";
+    } else {
+        this.state = false;
+        this.gameOver = true;
+
+        body[0].innerHTML = "<h1>Game over</h1>" +
+            "<p>Oh no, if you want to play again simply refresh the page.</p>";
+    }
+};
 
 /**
  * Character Superclass Constructor
@@ -55,7 +160,8 @@ Enemy.prototype.constructor = Enemy;
 Enemy.prototype.update = function(dt) {
     // Multiply any movement by the dt parameter to make sure
     // the game runs at the same speed for all computers.
-    this.x = this.x + (this.speed * dt);
+    if(game.state)
+        this.x = this.x + (this.speed * dt);
 
     // Check if enemy has reached the right side
     // if so, reset its x position
@@ -69,7 +175,7 @@ Enemy.prototype.update = function(dt) {
  */
 var Player = function(x, y) {
     Character.call(this, 'images/char-boy.png', x, y);
-    //this.lives = 3;
+    this.lives = 3;
 }
 Player.prototype = Object.create(Character.prototype);
 Player.prototype.constructor = Player;
@@ -84,53 +190,28 @@ Player.prototype.update = function() {
     allEnemies.forEach(function(enemy) {
         if(player.y - enemy.y == 9) {
             if(player.x < enemy.x + 75 && player.x + 75 > enemy.x) {
-                player.reset();
+                player.handleLives();
             }
         }
     });
 };
 
-Player.prototype.handleInput = function(key) {
-    switch(key) {
-        case 'up':
-            if(player.y < ROW_HEIGHT){
-                player.reset();
-            } else if(player.y > LIMIT_TOP){
-                player.y -= ROW_HEIGHT;
-            }
-            break;
-
-        case 'down':
-            if(player.y < LIMIT_BOTTOM)
-                player.y += ROW_HEIGHT;
-            break;
-
-        case 'left':
-            if(player.x > LIMIT_LEFT)
-                player.x -= COLUMN_WIDTH;
-            break;
-
-        case 'right':
-            if(player.x < LIMIT_RIGHT)
-                player.x += COLUMN_WIDTH;
-            break;
+Player.prototype.handleLives = function() {
+    if(this.lives > 0) {
+        this.lives--;
+        this.reset();
+        game.renderGameInfo();
+    } else {
+        game.end();
     }
 };
 
 // Instantiate objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [];
-for(var i = 1; i < 4; i++){
-    allEnemies.push(new Enemy(0 - (i * COLUMN_WIDTH), i * ROW_HEIGHT));
-}
-for(var i = 1; i < 4; i++){
-    allEnemies.push(new Enemy(0 - (i * COLUMN_WIDTH), i * ROW_HEIGHT));
-}
-for(var i = 1; i < 4; i++){
-    allEnemies.push(new Enemy(0 - (i * COLUMN_WIDTH), i * ROW_HEIGHT));
-}
-var player = new Player(303, 404);
+var game = new Game();
+var allEnemies = game.enemies;
+var player = game.player;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -139,8 +220,9 @@ document.addEventListener('keyup', function(e) {
         37: 'left',
         38: 'up',
         39: 'right',
-        40: 'down'
+        40: 'down',
+        80: 'pause'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    game.handleInput(allowedKeys[e.keyCode]);
 });
